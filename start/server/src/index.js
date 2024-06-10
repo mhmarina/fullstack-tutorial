1f0eb6fdcb564eefbc1ab96e55242168fe777b73
Marina <140115877+mhmarina@users.noreply.github.com>
2024-06-10 13:50:40 -0700

    Connecting to Data Sources

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,1 +1,28 @@
-require('dotenv').config();
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./schema");
+const { createStore } = require("./utils"); //highlight-line
+
+//highlight-start
+const LaunchAPI = require("./datasources/launch");
+const UserAPI = require("./datasources/user");
+//highlight-end
+
+const store = createStore(); //highlight-line
+
+const server = new ApolloServer({
+    typeDefs,
+    //highlight-start
+    dataSources: () => ({
+        launchAPI: new LaunchAPI(),
+        userAPI: new UserAPI({ store }),
+    }),
+    //highlight-end
+});
+
+server.listen().then(() => {
+    console.log(`
+    Server is running!
+    Listening on port 4000
+    Explore at https://studio.apollographql.com/sandbox
+  `);
+});
